64091b5fbdd80a59c71f84dad19991e4b8ca0d46
martin dahl <marda545@student.liu.se>
2021-10-28 13:47:25 +0100

    Cleaned up main, added train module

--- project1/scripts/main.py
+++ project1/scripts/main.py
@@ -8,47 +8,39 @@ from numpy.core.numeric import NaN
 from helpers import *
 #from logistic_implementations import logistic_gradient_descent, stoc_logistic_gradient_descent, calculate_logistic_loss
 from implementations import reg_logistic_regression, split_data, logistic_regression
+from train import train_model
 DATA_TRAIN_PATH = '../data/train.csv'
 DATA_TEST_PATH = '../data/test.csv'
 
+# GET TRAIN AND TEST DATA
 t0 = time.time()
 print("Loading data with really slow given function instead of fast Pandas...")
-# GET TRAIN AND TEST DATA
 y_train, x_train,  idx_train = load_csv_data(DATA_TRAIN_PATH)
 yb, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
-print("Finally completed loading data really slow with given function instead of fast Pandas!")
-print("That was {} seconds of your life!".format(int(time.time() - t0)))
+print("Finally completed loading data!\nThat was {} seconds! Pandas does the same in 6 seconds zipped!".format(int(time.time() - t0)))
 
-print("Fixing features...")
 # FIX FEATURES FOR TRAINING
+print("Fixing features...")
 x_train, x_test = create_features(x_train, tX_test)
 x_train,y_train,my_x_test,my_y_test = split_data(x_train, y_train, 0.5, seed=7)
 print("Features fixed!")
-
 
 # HYPERPARAMETERS
 hyperparameters = {
     "gamma":1e-4,
     "lambda":0,
+    "max_iters" : 100000,
+    "initial_w" : np.zeros(len(x_train[0])),
+    "conv_limit" : 1e-10
 }
-gamma = 1e-4
-_lambda = 0
-max_iters = 100000
-initial_w = np.zeros(len(x_train[0]))
-final_w = initial_w
-loss = np.nan
-conv_limit = 1e-10
 
 # TRAIN
 print("Training...")
-while (np.isnan(loss)):
-    w, loss = logistic_regression(y_train, x_train, initial_w, max_iters, gamma, conv_limit)
-    gamma = gamma/10
-    final_w = w
+w,loss = train_model(y_train, x_train, hyperparameters, algorithm = "logistic_regression")
 print("Training complete!")
 
+# TEST TRAINED MODEL
 print("Testing...")
-# TEST TRAIN
 print(final_w)
 my_y_pred = predict_labels(final_w, my_x_test)
 print("Testing complete!")
---
+++ project1/scripts/train.py
@@ -0,0 +1,19 @@
+from implementations import reg_logistic_regression, logistic_regression
+import numpy as np
+
+def train_model(y_train, x_train, hyperparameters, algorithm = "logistic_regression", multimodel = False):
+
+    if algorithm == "logistic_regression":
+        loss = np.NaN
+        gamma = hyperparameters["gamma"]
+        initial_w = hyperparameters["initial_w"]
+        max_iters = hyperparameters["max_iters"]
+        conv_limit = hyperparameters["conv_limit"]
+        while (np.isnan(loss)):
+            w, loss = logistic_regression(y_train, x_train, initial_w, max_iters, gamma, conv_limit)
+            gamma = gamma/10
+            final_w = w     
+    else:
+        print("Invalid train algorithm selected")
+
+    return w, loss
