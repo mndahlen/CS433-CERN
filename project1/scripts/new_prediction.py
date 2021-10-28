21ace2fb3af016a03413976582a3a37eb1f4ca96
Arvid Linder <arvli383@student.liu.se>
2021-10-28 14:20:49 +0200

    Just some more testing with the slow algorithm

--- project1/scripts/new_prediction.py
+++ project1/scripts/new_prediction.py
@@ -21,7 +21,7 @@ print(x_train[0])
 ## Fix with the train data. Remove some features, augment some other. Do the same with test matrix
 remove_vars = [2, 4, 7, 8, 9, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
 
-augment_vars = [3, 2, 1, 1, 2, 3, 3, 2]
+augment_vars = [3, 1, 1, 1, 2, 1, 4, 1]
 x_train, mean_x, std_x = standardize(x_train)
 x_test, mean_x_test, std_x_test = standardize(tX_test)
 x_train = remove_variables(x_train, remove_vars)
@@ -29,7 +29,7 @@ x_test = remove_variables(x_test, remove_vars)
 x_train = augment(x_train, augment_vars)
 x_test = augment(x_test, augment_vars)
 
-#x_train,y_train,my_x_test,my_y_test = split_data(x_train, y_train, 0.5, seed=7)
+x_train,y_train,my_x_test,my_y_test = split_data(x_train, y_train, 0.5, seed=7)
 x_train = np.array(x_train)
 ## 
 
@@ -42,11 +42,15 @@ w_submitted = [ 1.84218317e+01, -1.07724799e+01, -8.28169071e+00, -6.65540347e-0
   7.24767877e-02,  4.62745843e-01,  1.81722650e-02,  4.65404928e-01,
  -5.52326796e-02]
 
+w_regula = [11.16703908, -6.08146579, -4.80851339, -0.63012265, -0.04354135,  1.08390525,
+  2.96881392, -4.21631327, -0.10367624,  0.23483634,  0.46485023, -0.07612583,
+ -0.01322689,  0.37786607]
 gamma = 1e-6
-_lambda = 100
-max_iters = 100000
+_lambda = 0
+max_iters = 10000
 #initial_w = np.zeros(len(x_train[0]))
-initial_w = np.array(w_submitted)
+#initial_w = np.array(w_submitted)
+initial_w = np.array(w_regula)
 final_w = initial_w
 loss = np.nan
 conv_limit = 1e-8
@@ -59,13 +63,13 @@ while (np.isnan(loss)):
 
 # Finished with training
 print(final_w)
-#my_y_pred = predict_labels(final_w, my_x_test)
+my_y_pred = predict_labels(final_w, my_x_test)
 
-#correct = 0
-#for i, y in enumerate(my_y_pred):
-#    if ((y == my_y_test[i]) or (y == -1 and my_y_test[i] == 0)):
-#        correct += 1
-#print("Correct percentage on test set: {co}".format(co=correct/len(my_y_pred)))
+correct = 0
+for i, y in enumerate(my_y_pred):
+    if ((y == my_y_test[i]) or (y == -1 and my_y_test[i] == 0)):
+        correct += 1
+print("Correct percentage on test set: {co}".format(co=correct/len(my_y_pred)))
 
 y_pred = predict_labels(final_w, x_test)
 
